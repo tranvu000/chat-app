f6eebf60c417a6f588b4bdc1e27157bd3a461ca2
Vu Tran <tranvietvu0218@gmail.com>
2024-01-31 16:55:21 +0700

    ui-protecting-routes

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 import { baseUrl, postRequest } from "../utils/service";
 
 export const AuthContext = createContext();
@@ -12,6 +12,12 @@ export const AuthContextProvider = ({ children }) => {
     email: '',
     password: ''
   });
+console.log(user);
+  useEffect(() => {
+    const user = localStorage.getItem("User");
+
+    setUser(JSON.parse(user));
+  }, []);
 
   const updateRegisterInfo = useCallback((info) => {
     setRegisterInfo(info)
