9140f095318f4d937400f783ea20cf0c87379cf7
Vu Tran <tranvietvu0218@gmail.com>
2024-01-31 16:14:52 +0700

    register-user

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,11 +3,11 @@ import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
 import { AuthContext } from "../context/AuthContext";
 
 const Register = () => {
-  const {registerInfo, updateRegisterInfo} = useContext(AuthContext);
+  const {registerInfo, updateRegisterInfo, registerUser, registerError, isRegisterLoading} = useContext(AuthContext);
 
   return (
     <>
-      <Form>
+      <Form onSubmit={registerUser}>
         <Row style={{ height: "100vh", justifyContent: "center", paddingTop: "10%" }}>
           <Col xs={6}>
             <Stack gap={3}>
@@ -28,11 +28,13 @@ const Register = () => {
                 onChange={(e) => updateRegisterInfo({...registerInfo, password: e.target.value})}
               />
               <Button variant="primary" type="submit">
-                Register
+                {isRegisterLoading ? "Creating your account" : "Register"}
               </Button>
-              <Alert variant="danger">
-                <p>An error occured</p>
-              </Alert>
+              {registerError?.error && (
+                <Alert variant="danger">
+                  <p>{registerError?.message}</p>
+                </Alert>
+              )}
             </Stack>
           </Col>
         </Row>
