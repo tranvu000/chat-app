97c77945b0a803ffe8347ad3a2b663bddf38d488
Vu Tran <tranvietvu0218@gmail.com>
2024-01-31 08:54:42 +0700

    ui-login-register

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,28 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap";
+
 const Login = () => {
   return (
-    <>Login</>
+    <>
+      <Form>
+        <Row style={{ height: "100vh", justifyContent: "center", paddingTop: "10%" }}>
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2>Login</h2>
+
+              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="password" placeholder="Password" />
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+
+              <Alert variant="danger">
+                <p>An error occured</p>
+              </Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
   )
 };
 
