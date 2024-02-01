6507dee7f54c790c6c8b3957842111548f89e3c0
Vu Tran <tranvietvu0218@gmail.com>
2024-02-01 21:45:25 +0700

    ui-create-chats

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState } from "react";
-import { baseUrl, getRequest } from "../utils/service";
+import { createContext, useCallback, useEffect, useState } from "react";
+import { baseUrl, getRequest, postRequest } from "../utils/service";
 
 export const ChatContext = createContext();
 
@@ -7,6 +7,35 @@ export const ChatContextProvider = ({ children, user }) => {
   const [userChats, setUserChats] = useState(null);
   const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
   const [userChatsError, setUserChatsError] = useState(null);
+  const [potentialChats, setPotentialChats] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await getRequest(`${baseUrl}/users`);
+
+      if (response.error) {
+        return console.log("Error fetching users", response);
+      }
+
+      const pChats = response.filter((u) => {
+        let isChatCreated = false;
+
+        if (user?._id === u._id) return false;
+
+        if (userChats) {
+          isChatCreated = userChats?.some((chat) => {
+            return chat.members[0] === u._id || chat.members[1] === u._id;
+          });
+        }
+
+        return !isChatCreated;
+      });
+
+      setPotentialChats(pChats);
+    };
+
+    getUsers();
+  }, [userChats]);
 
   useEffect(() => {
     const getUserChats = async () => {
@@ -29,12 +58,24 @@ export const ChatContextProvider = ({ children, user }) => {
     getUserChats();
   }, [user]);
 
+  const createChat = useCallback( async (firstId, secondId) => {
+    const response = await postRequest(`${baseUrl}/chats`, JSON.stringify({firstId, secondId}));
+
+    if (response.error) {
+      return console.log("Error fetching chats", response);
+    }
+
+    setUserChats((prev) => [...prev, response]);
+  }, [])
+
   return (
     <ChatContext.Provider
       value={{
         userChats,
         isUserChatsLoading,
-        userChatsError
+        userChatsError,
+        potentialChats,
+        createChat
       }}
     >
       {children}
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -3,6 +3,7 @@ import { ChatContext } from "../context/ChatContext";
 import { AuthContext } from "../context/AuthContext";
 import { Container, Stack } from "react-bootstrap";
 import UserChat from "../components/chat/UserChat";
+import PotentialChats from "../components/chat/PotentialChats";
 
 const Chat = () => {
   const { user } = useContext(AuthContext);
@@ -10,6 +11,7 @@ const Chat = () => {
 
   return (
     <Container>
+      <PotentialChats/>
       {userChats?.length < 1 ? null : (
         <Stack direction="horizontal" gap={4} className="align-items-start">
           <Stack className="messages-box flex-grow-0 pe-3" gap={3}>
