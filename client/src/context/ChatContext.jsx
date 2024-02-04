0ac730bda86f6cc85d16bde88a84ab12fe76472d
Vu Tran <tranvietvu0218@gmail.com>
2024-02-04 15:45:48 +0700

    ui-send-messages

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -12,6 +12,8 @@ export const ChatContextProvider = ({ children, user }) => {
   const [messages, setMessages] = useState(null);
   const [isMessagesLoading, setIsMessagesLoading] = useState(false);
   const [messagesError, setMessagesError] = useState(null);
+  const [sendTextMessageError, setSendTextMessageError] = useState(null);
+  const [newMessage, setNewMessage] = useState(null);
 
   useEffect(() => {
     const getUsers = async () => {
@@ -81,6 +83,26 @@ export const ChatContextProvider = ({ children, user }) => {
     getMessages();
   }, [currentChat]);
 
+  const sendTextMessage = useCallback( async (textMessage, sender, currentChatId, setTextMessage) => {
+    if (!textMessage) {
+      return console.log("You must type something");
+    }
+
+    const response = await postRequest(`${baseUrl}/messages`, JSON.stringify({
+      chatId: currentChatId,
+      senderId: sender._id,
+      text: textMessage
+    }));
+
+    if (response.error) {
+      return setSendTextMessageError(response);
+    }
+
+    setNewMessage(response);
+    setMessages((prev) => [...prev, response]);
+    setTextMessage("");
+  }, [])
+
   const updateCurrentChat = useCallback((chat) => {
     setCurrentChat(chat);
   }, []);
@@ -106,7 +128,9 @@ export const ChatContextProvider = ({ children, user }) => {
         updateCurrentChat,
         messages,
         isMessagesLoading,
-        messagesError
+        messagesError,
+        currentChat,
+        sendTextMessage
       }}
     >
       {children}
